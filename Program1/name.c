493283355ff9af39919f1371cce151683570fa1d
Jeffrey Lansford <lansfordjr14@gmail.com>
2020-10-26 16:24:59 -0600

    added newer version to repo

--- Program1/name.c
+++ Program1/name.c
@@ -6,6 +6,8 @@
     Output: 
         JEFFREY LANSFORD
         HELLO
+        WORLD
+        memory locations main,foo,A,B,C,D, and S
     Preconditions: None
     Postconditions: None
 */
@@ -28,12 +30,11 @@ int B = 20;
 // function for memory test
 int foo() {
     int E[20];
-    printf("Location of          E is %20u\n", E);
 }
 
 int main() {
     // Integer Array to store bytes of integers
-    int A[100];
+    static int A[100];
     // Pointer to print integer array as string
     char *S;
     // Fill array with my name and hello for testing 0 byte
@@ -42,26 +43,30 @@ int main() {
     A[2] = 'L' + ('A' * square_256(1)) + ('N' * square_256(2)) + ('S' * square_256(3));
     A[3] = 'F' + ('O' * square_256(1)) + ('R' * square_256(2)) + ('D' * square_256(3));
     A[4] = '\n' + ('H' * square_256(1)) + ('E' * square_256(2)) + ('L' * square_256(3));
-    A[5] = 'L' + ('O' * square_256(1));
+    A[5] = 'L' + ('O' * square_256(1)) + ('\n' * square_256(2)) + ('W' * square_256(3));
 
     // Line for testing 0 byte
-    // A[5] = 'L' + (0 * square_256(1)) + ('O' * square_256(2));
+    // A[5] = 'L' + (0 * square_256(1)) + ('O' * square_256(2)) + ('\n' * square_256(3));
+
+    // fill rest of array to unsure test has worked
+    A[6] = 'O' + ('R' * square_256(1)) + ('L' * square_256(2)) + ('D' * square_256(3));
 
     // fills element to make sure that it does not print anything else
-    A[6] = 0;
+    A[7] = 0;
+
     // sets char pointer to integer array
     S = A;
     // prints as char array
     printf("%s\n", S);
     // test for memory location of variables
-    // char *C = malloc(100);
-    // int D[10];
-    // printf("Location of       main is %20u\n", main);
-    // printf("Location of square_256 is %20u\n", square_256);
-    // printf("Location of          B is %20u\n", &B);
-    // printf("Location of          C is %20u\n", C);
-    // printf("Location of          D is %20u\n", D);
-    // printf("Location of          S is %20u\n", &S);
-    // printf("Location of          A is %20u\n", A);
-    // foo();
+    char *C = malloc(100);
+    static int D[10];
+    printf("Location of       main is %20u\n", main);
+    printf("Location of        foo is %20u\n", foo);
+    printf("Location of          B is %20u\n", &B);
+    printf("Location of          D is %20u\n", D);
+    printf("Location of         *C is %20u\n", C);
+    printf("Location of          S is %20u\n", &S);
+    printf("Location of          A is %20u\n", A);
+    foo();
 }
